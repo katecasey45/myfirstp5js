ccfb5cb9ed0c374c8d4e94000deb8a3ddd43ec32
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-09-11 17:36:09 +0200

    class practice

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  fill("red");
+  noStroke();
 }
 
 function draw() {
-  background(125);
+  background(255,125,0);
+ rect(200, 200, 100, 100);
+
+ fill("green ");
+ ellipse(20, 20, 20,);
+ 
 }
+
