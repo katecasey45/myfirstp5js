a51bf8b5c51a8d459113aad5a314b359a650bb84
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-09-13 04:11:55 +0200

    assignment done

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
 function setup() {
   createCanvas(400, 400);
-  rectMode(CENTER);
-  fill("red");
-  noStroke();
+  background(0);
+ 
 }
 
 function draw() {
-  background(255,125,0);
- rect(200, 200, 100, 100);
-
- fill("green ");
- ellipse(20, 20, 20,);
+noStroke()
+  fill(0,0,200)
+ rect(0, 300, 100, 100);
  
+  fill(255,255,255)
+ rect(110,300,220,100)
+ 
+ fill(255,255,255)
+ rect(342,300,300,50)
+
+ fill(255,255,0)
+ rect(342,365,300,50)
+
+ fill(255,0,0)
+ rect(110,0,300,284)
+
+ fill(255,255,255)
+ rect(0,0,100,200)
+
+ fill(255,255,255)
+ rect(0, 215, 100, 69);
 }
 
